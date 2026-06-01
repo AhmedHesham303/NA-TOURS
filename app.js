2013bdd8d2216ead207e47508c61307bd380226a
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-06-01 11:25:17 +0300

    setting basic routing

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello');
+const fs = require('node:fs');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`);
+
+const tours = JSON.parse(data);
+
+app.use(express.json());
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('you can post');
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`running on port ${port}...`);
+  console.log(`running on port number ${port}...`);
 });
