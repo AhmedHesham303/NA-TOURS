a8cb51b5414bc0a8d1248992c9f6e4a2b370d2dd
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-06-03 15:16:48 +0300

    refactouring routes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const tours = JSON.parse(data);
 
 app.use(express.json());
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -18,8 +18,8 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
-app.get('/api/v1/tours/:id', (req, res) => {
+};
+const getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((t) => t.id === id);
 
@@ -35,13 +35,12 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
+};
+const updateTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((t) => t.id === id);
   const updatedTour = { ...tour, ...req.body };
-  const tourIndex = tours.findIndex((t) => t.id === parseInt(id));
+  const tourIndex = tours.findIndex((t) => t.id === id);
   tours[tourIndex] = updatedTour;
 
   fs.writeFile(
@@ -63,11 +62,11 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       });
     },
   );
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id;
-  const tourIndex = tours.findIndex((t) => t.id === parseInt(id));
+const deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tourIndex = tours.findIndex((t) => t.id === id);
 
   if (tourIndex === -1) {
     return res.status(404).json({
@@ -103,7 +102,37 @@ app.delete('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Failed to save tour',
+        });
+      } else {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: newTour,
+          },
+        });
+      }
+    },
+  );
+};
+
+app.get('/api/v1/tours', getAllTours);
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
 
 const port = 3000;
 
