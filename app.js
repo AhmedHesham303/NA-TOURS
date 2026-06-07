17e0053043783ad6a6d6a63ecf1f684a872b4095
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-06-07 19:46:47 +0300

    creating and mounting multiple routes

--- app.js
+++ app.js
@@ -175,15 +175,16 @@ const deleteUser = (req, res) => {
     },
   });
 };
+const tourRouter = express.Router();
+const userRouter = express.Router();
+app.use('/api/v1/tours', tourRouter);
 
-app.route(`${baseUrl}`).get(getAllTours).post(createTour);
-app.route(`${baseUrl}/:id`).get(getTour).patch(updateTour).delete(deleteTour);
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+app.use('/api/v1/users', userRouter);
+
+tourRouter.route(`/`).get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 const port = 3000;
 
 app.listen(port, () => {
