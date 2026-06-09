70ecbae6db44fb33763939b77c7ac2c32fd2a981
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-06-09 18:39:39 +0300

    serving static files

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const fs = require('node:fs');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const morgan = require('morgan');
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-
+app.use(rexpress.static(`${__dirname}/public`));
 const baseUrl = '/api/v1/tours';
 
 app.use('/api/v1/tours', tourRouter);
