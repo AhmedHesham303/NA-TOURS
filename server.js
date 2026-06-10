9f626036add798a60bf5c7085bc3e84d1a1a3212
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-06-10 16:31:51 +0300

    create basic tour model + it's schema

--- server.js
+++ server.js
@@ -13,8 +13,26 @@ mongoose
     console.error('DB connection error:', err.message);
     process.exit(1);
   });
-
 const app = require('./app');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name field is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price field is required'],
+  },
+});
+
+const Tour mongoose.model('Tour',tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`running on port number ${port}...`);
