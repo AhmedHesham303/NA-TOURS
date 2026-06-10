46f6ddd369db32bf1572481c8048fc7b87461814
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-06-10 15:58:34 +0300

    added gitiignore

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
 const DB = process.env.DATABASE;
+
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then((con) => {
-    console.log(con.connections);
+  .connect(DB)
+  .then(() => {
     console.log('DB connected');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err.message);
+    process.exit(1);
   });
+
 const app = require('./app');
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
